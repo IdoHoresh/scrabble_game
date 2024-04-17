07915db88ef165d43df663da96a2586f255f32b2
IdoHoresh <142943758+IdoHoresh@users.noreply.github.com>
2024-04-17 13:19:27 +0300

    added the board class as a singletone
    added the Word class that is an array of tiles that represent a word

--- Board.java
+++ Board.java
@@ -2,5 +2,27 @@ package test;
 
 
 public class Board {
+    private int[][] GameBoard;
+    private static Board singleBoard;
+
+    private Board(){
+        GameBoard = new int[15][15];
+    }
+
+    public static Board getBoard(){
+        if(singleBoard == null){
+            singleBoard = new Board();
+        }
+            return singleBoard;
+    }
+
+    public Tiles[][] getTiles(){
+
+    }
+
+
+    public boolean boardLegal(Word word){
+
+    }
 
 }
--- Word.java
+++ Word.java
@@ -1,7 +1,53 @@
 package test;
 
 
-public class Word {
+import java.util.Arrays;
+import java.util.Objects;
 
-	
+public class Word {
+    private Tile[] tiles;
+    private int row;
+    private int col;
+	private boolean vertical;
+
+    public Word(Tile[] tiles,int row, int col , boolean vertical){
+        if ((row>=0 && row<14)&&(col>=0 && col<14)) {
+            this.tiles = tiles.clone();
+            this.row = row;
+            this.col = col;
+            this.vertical = vertical;
+        }else{
+            return;
+        }
+
+    }
+
+    public Tile[] getTiles() {
+        return tiles;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    public boolean isVertical() {
+        return vertical;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Word word = (Word) o;
+        return row == word.row && col == word.col && vertical == word.vertical && Objects.deepEquals(tiles, word.tiles);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Arrays.hashCode(tiles), row, col, vertical);
+    }
 }
